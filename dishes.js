96a6f07a48a3f1b9816018e6bfd23dc5440d6ff1
Shyam Prasad <Shyam Prasad>
2015-11-08 18:06:30 +0530

    modules idli and dosa

---
+++ dishes.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Shyam Prasad on 11/8/2015.
+ */
+function printdosa(){
+    console.log("dosa: 35rs");
+
+}
+
+function printidli(){
+
+    console.log("idli: 25rs");
+}
+
+module.exports.dosa = printdosa;
