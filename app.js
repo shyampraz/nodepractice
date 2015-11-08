21bab0c2c449a248df34fbf1f0ad15df5ff54875
Shyam Prasad <Shyam Prasad>
2015-11-08 15:16:10 +0530

    prototypes shyam vidhya hit heal game

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-var goolibib = {
+function setuserstats(){
 
-    check: function(){
-        console.log("Goolibib is a company");
-        console.log(this === goolibib);
+    this.life=100;
+    this.hp=2;
+    this.hitothers=function(opponet){
+        opponet.life -= this.hp;
     }
+
 }
 
-function check(){
-    console.log("Microsoft is a bigger company");
-    console.log(this === global);
+var shyam = new setuserstats();
+var vidhya = new setuserstats();
+
+console.log("Shyam hits vidhya");
+shyam.hitothers(vidhya);
+console.log("Shyam's life: ",shyam.life);
+console.log("Vidhya's life: ",vidhya.life);
+
+console.log("Vidhya hits him back twice");
+vidhya.hitothers(shyam);
+vidhya.hitothers(shyam);
+console.log("Shyam's life: ",shyam.life);
+console.log("Vidhya's life: ",vidhya.life);
+
+setuserstats.prototype.healfactor=6;
+setuserstats.prototype.givelife=function(opponet){
+    opponet.life += this.healfactor;
 }
 
-goolibib.check();
-check();
+console.log("Shyam heals vidhya");
+shyam.givelife(vidhya);
+console.log("Shyam's life: ",shyam.life);
+console.log("Vidhya's life: ",vidhya.life);
 
+console.log("vidhya heals shyam");
+vidhya.givelife(shyam);
+console.log("Shyam's life: ",shyam.life);
+console.log("Vidhya's life: ",vidhya.life);
