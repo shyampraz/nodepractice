502c6d6ef848541ba0c634396d8a4d0f06f49984
Shyam Prasad <Shyam Prasad>
2015-11-09 18:26:46 +0530

    web server omg

--- server.js
+++ server.js
@@ -2,12 +2,25 @@
  * Created by Shyam Prasad on 11/9/2015.
  */
 var http = require("http");
+var fs= require("fs");
+
+
+function set404(response){
+
+    response.write("404: webpage not found");
+    response.end();
+
+}
 
 function onRequest(request,response){
-    console.log("Server connected to client");
-    //response.writeHead(200,{"content-type":"text/plain"});
-    response.write("hello how are you");
-    response.end();
+
+    if(request.method == "GET" && request.url == "/") {
+        console.log("Server connected to client");
+        //response.writeHead(200,{"content-type":"text/plain"});
+        fs.createReadStream("./index.html").pipe(response);
+    }else{
+        set404(response);
+    }
 
 }
 
